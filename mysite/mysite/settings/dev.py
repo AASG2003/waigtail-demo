f34dd903a799b191985889e436c269fd183eb3b2
Andrei <betoxd2003@gmail.com>
2024-02-25 20:07:55 -0400

    Instalando wagtail, welcome page y admin page

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xu*($c37s(c9a2si=b82n5_y&twfak+otp3u(68q3t#2dmjoh8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
